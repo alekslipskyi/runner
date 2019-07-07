781f84c2dfa8a36680319e1f92e8cd01b81f6247
alex <alex.lipskiy@do-it.co>
2019-07-07 21:50:31 +0300

    refactor code

--- core/index.cpp
+++ core/index.cpp
@@ -22,15 +22,15 @@ char* arguments = (char *)"-c";
 
 Core::Core(int argc, char *argv[]) {
     for (int i = 1; i < argc; i++) {
-        this->findCandidate(string(argv[i]));
+        this->findCandidate(argv[i]);
     }
 
     if (strlen(this->path_to_watch.c_str()) == 0 && strlen(this->path_to_watch_file.c_str()) == 0) {
-        this->error(errMessages[errors::NO_PATH_TO_WATCH]);
+        this->error(&errMessages[errors::NO_PATH_TO_WATCH]);
     }
 
     if (strlen(this->command.c_str()) == 0) {
-        this->error(errMessages[errors::NO_COMMAND]);
+        this->error(&errMessages[errors::NO_COMMAND]);
     }
 
     if (strlen(this->path_to_watch.c_str()) != 0) {
@@ -40,20 +40,20 @@ Core::Core(int argc, char *argv[]) {
     }
 }
 
-void Core::error(string message) {
-    cout << "Error: " << message.c_str() << endl;
+void Core::error(const string* message) {
+    cout << "Error: " << message->c_str() << endl;
     exit(0);
 }
 
-bool Core::isCandidate(string candidate, int expectedCandidate) {
-    return candidate.substr(0, strlen(candidates[expectedCandidate].c_str())) == candidates[expectedCandidate];
+bool Core::isCandidate(const char* candidate, int expectedCandidate) {
+    return string(candidate).substr(0, strlen(candidates[expectedCandidate].c_str())) == candidates[expectedCandidate];
 }
 
-string Core::parseCandidate(string candidate, int needCandidate) {
-    return candidate.substr(strlen(candidates[needCandidate].c_str())+1, strlen(candidate.c_str()));
+string Core::parseCandidate(const char* candidate, int needCandidate) {
+    return string(candidate).substr(strlen(candidates[needCandidate].c_str())+1, strlen(string(candidate).c_str()));
 }
 
-void Core::findCandidate(string candidate) {
+void Core::findCandidate(const char* candidate) {
     if (this->isCandidate(candidate, enumCandidates::watchpath)) {
         this->path_to_watch = this->parseCandidate(candidate, enumCandidates::watchpath);
     }
@@ -70,8 +70,8 @@ void Core::findCandidate(string candidate) {
         this->path_to_config = this->parseCandidate(candidate, enumCandidates::config);
     }
 
-    if (this->isCandidate(candidate, enumCandidates::forcekill)) {
-        this->is_force_kill = true;
+    if (this->isCandidate(candidate, enumCandidates::port)) {
+        this->port = this->parseCandidate(candidate, enumCandidates::port);
     }
 }
 
@@ -85,12 +85,15 @@ void Core::jsonToEnv() {
 
     Value::Members members = json.getMemberNames();
 
-    for (int i = 0; i < members.size(); i++) {
-        setenv(members[i].c_str(), json[members[i]].asString().c_str(), 1);
+    for (auto &member : members) {
+        setenv(member.c_str(), json[member].asString().c_str(), 1);
     }
 }
 
 void Core::createProcess() {
+    if (strlen(this->port.c_str()) != 0) this->ensurePortIsFree();
+    this_thread::sleep_for(chrono::milliseconds(300));
+
     if (strlen(this->path_to_config.c_str()) != 0) {
         this->jsonToEnv();
     }
@@ -103,36 +106,16 @@ void Core::createProcess() {
 
     char *argv[] = {executor, arguments, _command, nullptr};
     posix_spawn(&_pid, "/bin/sh", nullptr, nullptr, argv, environ);
-    this->recognizePID(to_string(_pid));
 }
 
-void Core::recognizePID(string _pid) {
-    ifstream tmpPID("/tmp/runner_pid.txt");
-
-    system(("pgrep -P " + _pid + " > /tmp/runner_pid.txt").c_str());
-
-    this_thread::sleep_for(chrono::milliseconds(800));
-
-    string childPid;
-    string sLine;
-
-    getline(tmpPID, sLine);
-
-    if (strlen(sLine.c_str()) != 0) {
-        this->pid = sLine;
-        this->recognizePID(sLine);
-    }
+void Core::ensurePortIsFree() {
+    system(("kill $(lsof -t -i :"+this->port+")").c_str());
 }
 
-void Core::reloadProcess(string path_to_watch, FileStatus status) {
+void Core::reloadProcess(const string* _path_to_watch, FileStatus status) {
     if(strlen(this->path_to_watch.c_str()) != 0 &&
-       !filesystem::is_regular_file(filesystem::path(path_to_watch)) && status != FileStatus::erased) {
+       !filesystem::is_regular_file(filesystem::path(*_path_to_watch)) && status != FileStatus::erased) {
         return;
-    }
-
-    if (this->is_force_kill) {
-        cout << "pid are" << this->pid << endl;
-        system(("kill -9" + this->pid).c_str());
     }
 
     this->createProcess();
@@ -142,12 +125,12 @@ void Core::exec() {
     this->createProcess();
 
     if (strlen(this->path_to_watch.c_str()) == 0) {
-        this->fileWatcher->listenFile([&] (string path_to_watch, FileStatus status) -> void {
-            Core::reloadProcess(path_to_watch, status);
+        this->fileWatcher->listenFile([&] (string _path_to_watch, FileStatus status) -> void {
+            Core::reloadProcess(&_path_to_watch, status);
         });
     } else {
-        this->fileWatcher->listenPath([&] (string path_to_watch, FileStatus status) -> void {
-            Core::reloadProcess(path_to_watch, status);
+        this->fileWatcher->listenPath([&] (string _path_to_watch, FileStatus status) -> void {
+            Core::reloadProcess(&_path_to_watch, status);
         });
     }
 }
--- core/index.h
+++ core/index.h
@@ -9,29 +9,28 @@
 using namespace std;
 
 enum errors { NO_COMMAND, NO_PATH_TO_WATCH };
-enum enumCandidates { watchpath, watchfile, forcekill, exec, config };
+enum enumCandidates { watchpath, watchfile, port, exec, config };
 
 const string errMessages[] = {
         "please specify the command, alike --exec=\"ls\"",
         "env or arg PATH_TO_WATCH is required, alike --watch=./",
 };
-const string candidates[] = { "--watchpath", "--watchfile", "--forcekill", "--exec", "--config" };
+const string candidates[] = { "--watchpath", "--watchfile", "--port", "--exec", "--config" };
 
 class Core {
 private:
     string      command;
     string      path_to_config;
-    string      pid;
     string      path_to_watch;
     string      path_to_watch_file;
-    void        recognizePID(string pid);
-    bool        is_force_kill;
-    void        error(string message);
+    void        ensurePortIsFree();
+    string      port;
+    void        error(const string* message);
     void        createProcess();
-    void        reloadProcess(string path_to_watch, FileStatus status);
-    void        findCandidate(string candidate);
-    bool        isCandidate(string candidate, int expectedCandidate);
-    string      parseCandidate(string candidate, int needCandidate);
+    void        reloadProcess(const string* path_to_watch, FileStatus status);
+    void        findCandidate(const char* candidate);
+    bool        isCandidate(const char* candidate, int expectedCandidate);
+    string      parseCandidate(const char* candidate, int needCandidate);
     FileWatcher *fileWatcher;
 public:
     void        jsonToEnv();
